d580aedecd091da61b651326f9b69ecd9e9d9620
chalokim <chalokim@naver.com>
2020-11-30 09:33:36 +0900

    서버 이동

--- BusProject/Program.cs
+++ BusProject/Program.cs
@@ -22,7 +22,7 @@ namespace BusProject
 
             DbContextCreator.Context = () => new BusProjectEntities();
             
-            Application.Run(new ReservationScreen());
+            Application.Run(new Mainscreen());
 
         }
     }
